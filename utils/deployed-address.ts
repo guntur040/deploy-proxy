4a182eaebb3f82c4087d17f02f34e41b1e4e33f0
guntur040 wawan <hguntur09@gmail.com>
2024-08-17 02:51:20 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x16b62d5ab4f478EBc70214031E1B41dd03F2e3Db'
+export const ProxyAdmin = '0xb34F3B07D7a33ecAb8AFE538199ba0E72B72B0d1'
+export const SWTRImplementation = '0xA2AF804199A7732F12459EFB75b3EC31d51f692F'
     
